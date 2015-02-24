165ea709000369872a74a0e557e0ee795bf55501
karavar <karavar@gmail.com>
2015-02-23 16:51:25 -0800

    Exceptions added

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,46 +1,76 @@
 
-public class Percolation {	
-	
-	private boolean[][] grid; // false = blocked, true = open.
-		
-	 public Percolation(int N)               // create N-by-N grid, with all sites blocked	{
+public class Percolation { 
+ 
+ //private boolean[][] grid; // false = blocked, true = open.
+  WeightedQuickUnionUF wuf;
+  int gridSize;
+  
+  public Percolation(int N)               // create N-by-N grid, with all sites blocked {
+  {	  
+   	  if(N<=0)
+   	  {
+   		  throw new IllegalArgumentException();
+   	  }
+   	  else
+   	  {
+	  gridSize = N;
+   	  wuf = new WeightedQuickUnionUF(N*N); // this creates a single dimension array of size N*N.
+   	  }
+  }
+  
+  public int ijtok(int i,int j)
+  {
+	  return i*gridSize + j ;
+  }  
+  
+ 
+ public void open(int i, int j) // open site (row i, column j) if it is not open already
+ {
+	// open a site at i,j => create a union of (i-1,j), (i+1,j) (i,j-1),(i,j+1)
+	 if(i >= gridSize || i<0 || j>= gridSize || j<0)
 	 {
-		 for(int i=0;i<N;i++)
-		 {
-			 for(int j=0;j<N;j++)
-			 {
-				 grid[i][j] = false; //false = blocked
-			 }
-		 }
+		 throw new ArrayIndexOutOfBoundsException();
 	 }
-	
-	public void open(int i, int j) // open site (row i, column j) if it is not open already
-	{
-		
-	}
-	
-	public boolean isOpen(int i, int j) // is site (row i, column j) open?
-	{
-		
-		return true;
-	}	
-	
-	public boolean isFull(int i, int j) // is site (row i, column j) full?
-	{
-		
-		return true;
-	}
-	
-	public boolean percolates()   // does the system percolate?
-	{
-		
-		return true;
-	}
-	
-	public static void main(String[] args)
-	{
-		
-	}
-	
+	 else
+	 {
+		 	 
+		 int p = ijtok(i,j);
+		 wuf.union(p, ijtok(i-1,j));
+		 wuf.union(p, ijtok(i+1,j));
+		 wuf.union(p, ijtok(i,j-1));
+		 wuf.union(p, ijtok(i,j+1));
+		 
+		 
+	 }
+	 
+	 
+	 
+	   
+ }
+ 
+ public boolean isOpen(int i, int j) // is site (row i, column j) open?
+ {
+  
+  return true;
+ } 
+ 
+ public boolean isFull(int i, int j) // is site (row i, column j) full?
+ {
+  
+  return true;
+ }
+ 
+ public boolean percolates()   // does the system percolate?
+ {
+  
+  return true;
+ }
+ 
+ public static void main(String[] args)
+ {
+	 Percolation p = new Percolation(4);	 
+	 p.wuf.union(0, 10); 
+ }
+ 
 
 }
